3a65c74b31fb86148f2742b977ecd400951193cb
Orangee-Liu <37042164+lcning@users.noreply.github.com>
2023-07-24 13:37:21 +0000

    create run.py, utils.py

---
+++ code/decision/run.py
@@ -0,0 +1,22 @@
+from utils import optimal_portfolio, DE_portfolio
+
+class Test:
+    def __init__(self):
+        self.freq = '1min'
+        return
+
+    def preprocess_data(self):
+        return data
+
+
+    def run(self):
+        weights, returns, risks = optimal_portfolio(return_vec.to_numpy())
+
+        de = DE_portfolio(constraint_eq=[], 
+                            constraint_ueq=[
+                                            # 小于等于0
+                                            lambda x: sum([abs(i) for i in x]) - 1,
+                                            lambda x: 0.25 - sum([abs(i) for i in x])
+                                        ])
+
+        weights = de.run()
---
+++ code/decision/utils.py
@@ -0,0 +1,80 @@
+from sko.DE import DE
+
+def optimal_portfolio(returns):
+    # Markovitz
+    n = len(returns)
+    returns = np.asmatrix(returns)
+    
+    N = 100
+    mus = [10**(5.0 * t/N - 1.0) for t in range(N)]
+    
+    # Convert to cvxopt matrices
+    S = opt.matrix(np.cov(returns))
+    pbar = opt.matrix(np.mean(returns, axis=1))
+    
+    # Create constraint matrices
+    G = -opt.matrix(np.eye(n))   # negative n x n identity matrix
+    h = opt.matrix(0.0, (n ,1))
+    A = opt.matrix(1.0, (1, n))
+    b = opt.matrix(1.0)
+    
+    # Calculate efficient frontier weights using quadratic programming
+    portfolios = [solvers.qp(mu*S, -pbar, G, h, A, b)['x'] 
+                for mu in mus]
+    ## CALCULATE RISKS AND RETURNS FOR FRONTIER
+    returns = [blas.dot(pbar, x) for x in portfolios]
+    risks = [np.sqrt(blas.dot(x, S*x)) for x in portfolios]
+    ## CALCULATE THE 2ND DEGREE POLYNOMIAL OF THE FRONTIER CURVE
+    m1 = np.polyfit(returns, risks, 2)
+    x1 = np.sqrt(m1[2] / m1[0])
+    # CALCULATE THE OPTIMAL PORTFOLIO
+    wt = solvers.qp(opt.matrix(x1 * S), -pbar, G, h, A, b)['x']
+    return np.asarray(wt), returns, risks
+
+
+class DE_portfolio:
+    '''
+    min f(x1, ..., x100) = - annualized_IR(x1, ..., x100)
+    s.t.
+        0.25 <= abs(x1) + ... + abs(x100) <= 1
+        -1 <= x1, ..., x100 <= 1
+    '''
+    def __init__(self, ):
+        self.constraint_eq = []
+        self.constraint_ueq = constraint_ueq
+
+    @staticmethod
+    def obj_func(p):
+        ''' IR越大越好
+        '''
+        daily_returns = pd.read_csv('real_returns_0501.csv')[-20:].reset_index(drop=True).T
+        # daily_returns = pd.read_csv('real_returns.csv')[:10].T
+        daily_returns['ID'] = daily_returns.index.tolist()
+        decisions = pd.DataFrame({'decision': p, 'ID':daily_returns.index.tolist()})    
+        portfolio = daily_returns.merge(decisions, on='ID')
+        portfolio = portfolio[portfolio.decision != 0]
+        weighted_portfolio_return = pd.DataFrame((portfolio.iloc[:, :daily_returns.shape[1]-1].to_numpy().T * portfolio.decision.to_numpy()).T)
+        # print(weighted_portfolio_return)
+        portfolio_daily_return = weighted_portfolio_return.apply(lambda x: np.log(1 + x)).sum(axis=0)
+        # print(weighted_portfolio_return.sum(axis=0))
+
+        ret_T = sum(portfolio_daily_return)
+        D = portfolio_daily_return.shape[0]
+        var = np.sum([(x - ret_T / D)**2 for x in portfolio_daily_return.tolist()]) / (D-1)
+        sdp = np.sqrt(var)
+        annualized_IR = (D+1)/D * 12 * ret_T / np.sqrt(252) / sdp
+        return -annualized_IR
+
+    def run(self):
+        de = DE(func=obj_func, n_dim=100, size_pop=2000, max_iter=300, lb=[-1]*100, ub=[1]*100,
+    constraint_eq=constraint_eq, constraint_ueq=constraint_ueq)
+
+        
+        best_x, best_y = de.run()
+        print('best_y:', best_y)
+
+        best20_generation = np.array(de.generation_best_Y).argsort()[:20]
+
+        return weights
+
+    
