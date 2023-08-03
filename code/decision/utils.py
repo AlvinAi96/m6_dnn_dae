1c1fc46a633084f40a4017e80df8cf39460d9ce3
Orangee-Liu <37042164+lcning@users.noreply.github.com>
2023-08-03 07:56:54 +0000

    add monte_carlo, min_variance, risk_parity, update black_litterman

--- code/decision/utils.py
+++ code/decision/utils.py
@@ -84,43 +84,111 @@ class DE_portfolio:
         return best20_generation
 
 
-def black_litterman(return_vec):
+def black_litterman(returns):
+    n_asset = returns.shape[1]
+    cov = np.cov(returns.T)
+    volatility = np.sqrt(np.diag(cov))
+    risk_aversion_coefficient = 2.5
 
-    n = return_vec.shape[1]
-    return_vec = return_vec.to_numpy()
+    # Define the investor views on the asset returns (neutral position)
+    view_uncertainty = np.eye(n_asset) * 1e10
+    views = np.zeros(n_asset)
 
-    prior_returns = np.random.rand(n)
+    inv_cov = np.linalg.inv(cov)
+    # Calculate the implied equilibrium returns
+    implied_return = risk_aversion_coefficient * (cov @ np.ones((n_asset, 1)))
 
-    # views = np.random.rand(n)
-    views = np.random.rand(5)
-    # picked = np.random.choice(range(n), 5, replace=False)
+    # Calculate the Black-Litterman expected returns
+    bl_expected_return = np.linalg.inv(view_uncertainty + inv_cov) @ \
+                     (view_uncertainty @ implied_return + (inv_cov @ views)[:, np.newaxis])
+    optimal_weights = risk_aversion_coefficient * cov @ bl_expected_return
+    # Normalize the weights to sum up to 1
+    optimal_weights /= np.sum(optimal_weights)
+    return optimal_weights
 
-    P = np.identity(n)
-    tau = 0.05
+def risk_parity(return_vec, max_iter=10):
+    num_asset = return_vec.shape[1]
+    cov = np.cov(return_vec.T)
+    volatility = np.sqrt(np.diag(cov))
+    inv_cov = np.linalg.inv(cov)
 
-    Omega = np.transpose(P) * P
-    # posterior = np.matmul((tau * prior_returns + np.matmul(np.transpose(P), views)), 
-    #                         np.linalg.inv(tau*np.identity(len(Omega)) + Omega))
+    target_risk_contribution = 1 / num_asset
+    weights = np.ones(num_asset) / num_asset
+    tolerance = 1e-6
 
-    views_matrix = np.tile(views, (n, 1)) 
-    prior_returns = prior_returns[:, np.newaxis]
-    posterior = np.matmul( np.linalg.inv(tau*np.identity(n) + Omega), 
-                            (tau * prior_returns) + np.matmul(P, views_matrix))  
-                    
+    for i in range(max_iter):
+        risk_contribution = np.multiply(weights, volatility) @ inv_cov
+        diff_risk_contribution = risk_contribution - target_risk_contribution
+        gradient = 2 * inv_cov @ diff_risk_contribution
+        step_size = 1 / np.sqrt(np.dot(np.dot(gradient, cov), gradient))
+        weights -= step_size * gradient
+        if np.max(np.abs(diff_risk_contribution)) < tolerance:
+            break
+
+    return weights
 
 
+def min_variance(asset_returns):
+    n_assets = asset_returns.shape[1]
+    covariance_matrix = np.cov(asset_returns.T)
+    weights = np.linalg.inv(covariance_matrix) @ np.ones((n_assets, 1))
+    weights /= np.sum(weights)
+    return weights
 
-    cov = np.cov(np.transpose(return_vec))
-    init_weights = np.repeat(1/n, n)
+def monte_carlo(asset_returns, n_scenarios=10):
+    n_assets = asset_returns.shape[1]
+    cov = np.cov(asset_returns.T)
+    # Define the minimum acceptable return for the portfolio
+    min_acceptable_return = 0.05
 
-    def fitness(weights):
-        return -np.sum(np.dot(weights, posterior)) + 0.01 * np.mean(np.dot(weights, cov))
+    # Define the maximum acceptable risk (standard deviation) for the portfolio
+    max_acceptable_risk = 0.10
 
-    bounds = [(0, 1) for i in range(n)]
-    constraints = [{'type':'eq', 
-                    'fun': lambda w: 1 - np.sum(w)}]
+    # Calculate the optimal portfolio weights using the mean-variance optimization
+    weights = np.linalg.inv(cov) @ np.ones((n_assets, 1))
+    weights /= np.sum(weights)
 
-    optimized = minimize(fitness, init_weights, method='SLSQP', bounds=bounds, constraints=constraints)
+    # Run the Monte Carlo simulation
+    portfolio_returns = np.zeros(n_scenarios)
+    portfolio_risks = np.zeros(n_scenarios)
 
-    return optimized.x
+    for i in range(n_scenarios):
+        # Generate random returns for the portfolio based on the covariance matrix
+        random_returns = np.random.multivariate_normal(np.zeros(n_assets), cov)
 
+        # Calculate the return and risk of the portfolio for the current scenario
+        portfolio_return = weights.T @ random_returns
+        portfolio_risk = np.sqrt(weights.T @ cov @ weights)
+
+        # Store the return and risk values for each scenario
+        portfolio_returns[i] = portfolio_return
+        portfolio_risks[i] = portfolio_risk
+
+    # Identify the optimal portfolio using the simulated outcomes
+    optimal_portfolio_index = np.argmax(portfolio_returns)
+    optimal_portfolio_return = portfolio_returns[optimal_portfolio_index]
+    optimal_portfolio_risk = portfolio_risks[optimal_portfolio_index]
+    optimal_portfolio_weights = weights
+
+    # # Refine the optimal portfolio by adjusting the weights based on the simulated outcomes
+    # while (optimal_portfolio_return < min_acceptable_return) or (optimal_portfolio_risk > max_acceptable_risk):
+    #     # Generate random returns for the portfolio based on the covariance matrix
+    #     random_returns = np.random.multivariate_normal(np.zeros(n_assets), cov)
+
+    #     # Calculate the return and risk of the portfolio for the current scenario
+    #     portfolio_return = optimal_portfolio_weights.T @ random_returns
+    #     portfolio_risk = np.sqrt(optimal_portfolio_weights.T @ cov @ optimal_portfolio_weights)
+
+    #     # Adjust the weights based on the simulated outcomes
+    #     if portfolio_return < min_acceptable_return:
+    #         optimal_portfolio_weights *= 0.9
+    #     elif portfolio_risk > max_acceptable_risk:
+    #         optimal_portfolio_weights *= 1.1
+
+    #     # Normalize the weights to sum up to 1
+    #     optimal_portfolio_weights /= np.sum(optimal_portfolio_weights)
+
+    #     # Re-calculate the return and risk of the portfolio with the new weights
+    #     optimal_portfolio_return = optimal_portfolio_weights.T @ np.mean(asset_returns, axis=1)
+    #     optimal_portfolio_risk = np.sqrt(optimal_portfolio_weights.T @ cov @ optimal_portfolio_weights)
+    return optimal_portfolio_weights
